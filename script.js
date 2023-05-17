cb28fab519e29ddc0a664652243436c8c1876b2d
Bogdan Pokrepin <bogpok@ya.ru>
2023-05-17 17:07:14 +0300

    add assets + Ox movement

---
+++ script.js
@@ -0,0 +1,209 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 300;
+    canvas.height = 200;
+
+    let keysDict = [
+        'w',
+        'a',
+        's',
+        'd',
+    ];
+
+
+    class InputHandler {
+        constructor() {
+            // keep track of multiple keys presses
+            this.keys = [];
+
+            // with every instance event listener created
+            // using arrow fucntion inherits parent scope (lexical scoping)
+            window.addEventListener('keydown', e => {
+                // when any button is pressed
+
+                // element is not present in the array
+                if (keysDict.includes(e.key) && this.keys.indexOf(e.key) === -1 ) {
+                    this.keys.push(e.key);
+                }
+                console.log(this.keys);
+            });
+
+            window.addEventListener('keyup', e => {
+                if (keysDict.includes(e.key)) {
+                    // remove one element from array
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(this.keys);
+            });
+        }
+
+    }
+
+    class Player {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;     
+
+            
+            
+            this.img = {
+                height: 1792,
+                width: 7200,
+                states: [
+                    {name:'idle', n: 6},
+                    {name:'run', n: 8},
+                    {name:'j_up', n: 3},
+                    {name:'j_down', n: 3},
+                    {name:'atk_leg', n: 7},
+                    {name:'atk1', n: 6},
+                    {name:'atk2', n: 12},
+                    {name:'atk3', n: 23},
+                    {name:'atk_sp', n: 25},
+                    {name:'meditate', n: 16},
+                    {name:'roll', n: 6},
+                    {name:'guard', n: 13},
+                    {name:'takehit', n: 6},
+                    {name:'faint', n: 14},                    
+                ]
+            }
+            this.frame = {
+                width: this.img.width/25,
+                height: this.img.height/this.img.states.length,
+                current: 0,
+                state: 0,
+                flapinterval: 100,
+                timer: 0,
+            }
+            
+            this.image = playerImage;
+
+            // FIRST ANIMATION
+            this.hitbox = {
+                width: 25,
+                height: 35,
+            }
+
+            this.gameConfig = {
+                groundLevel: 20,
+            }
+            
+            //problem with the sprite COMPENSATE
+            this.frame.bound = {
+                x: 11 - this.frame.width/2,
+                y: this.hitbox.height+7 - this.frame.height,
+            }
+            
+            // x0 and y0
+            this.x = 0;
+            this.y = this.gameHeight - this.gameConfig.groundLevel - this.hitbox.height;           
+
+            this.position = {
+                x: this.x + this.frame.bound.x,
+                y: this.y + this.frame.bound.y,
+            }
+            
+            this.v = {
+                x: 0,
+                y: 0,
+                m: 0.1,
+            }
+            
+            
+        }
+        #changeFrame(dt){
+            if (this.frame.timer > this.frame.flapinterval) {
+                this.frame.current++;
+                this.frame.current = this.frame.current % this.img.states[this.frame.state].n;
+                this.frame.timer = 0;
+            } else {
+                this.frame.timer += dt;
+            }
+        }
+        update_x(dt, dx = 0, reset = false){
+            // horizontal movement
+            
+            if (reset) {
+                this.x = dx;
+                this.position.x = this.x + this.frame.bound.x;
+            } else {
+                this.x += dx;
+                this.position.x += dx;
+            }
+        }
+        update(dt){
+            this.#changeFrame(dt);
+            
+            this.update_x(dt, this.v.x*dt)
+
+            if (input.keys.indexOf('d') > -1) {
+                this.v.x = this.v.m;
+            } else if (input.keys.indexOf('a') > -1) {
+                this.v.x = -this.v.m;
+            } else {
+                this.v.x = 0;
+            }
+
+            
+            //#region boundaries
+            if (this.x < 0) this.update_x(dt, 0, true);
+            else if (this.x > this.gameWidth - this.hitbox.width) this.update_x(dt, this.gameWidth - this.hitbox.width, true);
+            //#endregion
+            
+            
+        }
+
+        draw(context){
+            // hitbox area
+            context.fillStyle = 'white';
+            context.fillRect(this.x, this.y, this.hitbox.width, this.hitbox.height);
+
+            // whole slide from spritesheet
+            context.strokeStyle = 'red';
+            context.strokeRect(this.position.x, this.position.y, this.frame.width, this.frame.height)
+            
+            
+            
+
+            context.drawImage(this.image, 
+                this.frame.current*this.frame.width, this.frame.state*this.frame.height, this.frame.width, this.frame.height,
+                this.position.x, this.position.y, this.frame.width, this.frame.height);
+        }
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+
+    }
+
+    function displayStatusText() {
+
+    }
+
+    //#region prepare for animate
+    const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+
+    let lastTime = 0;
+    //#endregion
+    function animate(timestamp) {
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+
+        dt = timestamp - lastTime;
+        lastTime = timestamp;
+
+        player.update(dt);
+        player.draw(ctx);
+        
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+});
